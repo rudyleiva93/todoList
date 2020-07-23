269e3f3bba06abce65bf481b0e905adca8647c6d
rudyleiva93 <leivarudy93@gmail.com>
2020-07-23 11:14:46 -0700

    Create customs lists using dynamic routing

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ const item3 = new Item ({
 
 const defaultItems = [item1, item2, item3];
 
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
 app.get("/", function(req, res) {
 
   Item.find(function(err, items) {
@@ -48,6 +55,29 @@ app.get("/", function(req, res) {
       res.render("list", {listTitle: "Today", newListItems: items});
     }
   });
+
+});
+
+app.get("/:customListName", function(req, res) {
+  const customListName = req.params.customListName;
+
+  List.findOne({name: customListName}, function(err, foundList) {
+    if(!err) {
+      if(!foundList) {
+
+        const list = new List ({
+          name: customListName,
+          items: defaultItems
+        });
+
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items})
+      }
+    }
+  });
+
 
 });
 
@@ -75,12 +105,6 @@ app.post("/delete", function(req, res){
       res.redirect("/");
     }
   });
-});
-
-app.get("/work", function(req, res) {
-
-  res.render("list", {listTitle: "Work List", newListItems: workItems });
-
 });
 
 app.get("/about", function(req, res) {
